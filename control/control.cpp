0c0ee519d4d7e948d2f2b29ab90b4558fc9c683d
Andrey Egorov <89473462+Arty525@users.noreply.github.com>
2023-05-12 19:04:57 +0300

    Add files via upload

---
+++ control/control.cpp
@@ -0,0 +1,17 @@
+﻿#include <QApplication>
+#include <QPushButton>
+#include "control.h"
+#include "./ui_control.h"
+
+int main(int argc, char* argv[])
+{
+	QApplication a(argc, argv);
+	CalculatorMainWindow window(nullptr);
+	Ui::MainWindow control;
+	control.setupUi(&window);
+	window.volume = control.volume;
+	window.chanel = control.chanel;
+	window.resize(200, 800);
+	window.show();
+	return QApplication::exec();
+}
--- control/control.h
+++ control/control.h
@@ -1,1 +1,41 @@
+﻿#pragma once
+#include <QMainWindow>
+#include <QtWidgets/QProgressBar>
+#include <QtWidgets/QTextBrowser>
+#include <string>
+class CalculatorMainWindow : public QMainWindow {
+	Q_OBJECT
 
+public:
+
+	QProgressBar* volume = nullptr;
+	QTextBrowser* chanel = nullptr;
+	
+
+	CalculatorMainWindow(QWidget* parent = nullptr) : QMainWindow(parent) {}
+
+public slots:
+	/*void plus() { result->setText(val1->text() + val2->text()); };
+	void minus() { result->setText(std::stoi(val1->text()) - std::stoi(val2->text())); };
+	void multiplication() { result->setText(val1->text() * val2->text()); };
+	void divide() { result->setText(val1->text() / val2->text()); };*/
+
+	void ch1() { if(chanel->toPlainText().size() < 3) chanel->setText(chanel->toPlainText() + "1"); };
+	void ch2() { if (chanel->toPlainText().size() < 3) chanel->setText(chanel->toPlainText() + "2"); };
+	void ch3() { if (chanel->toPlainText().size() < 3) chanel->setText(chanel->toPlainText() + "3"); };
+	void ch4() { if (chanel->toPlainText().size() < 3) chanel->setText(chanel->toPlainText() + "4"); };
+	void ch5() { if (chanel->toPlainText().size() < 3) chanel->setText(chanel->toPlainText() + "5"); };
+	void ch6() { if (chanel->toPlainText().size() < 3) chanel->setText(chanel->toPlainText() + "6"); };
+	void ch7() { if (chanel->toPlainText().size() < 3) chanel->setText(chanel->toPlainText() + "7"); };
+	void ch8() { if (chanel->toPlainText().size() < 3) chanel->setText(chanel->toPlainText() + "8"); };
+	void ch9() { if (chanel->toPlainText().size() < 3) chanel->setText(chanel->toPlainText() + "9"); };
+	void ch0() { if (chanel->toPlainText().size() < 3) chanel->setText(chanel->toPlainText() + "0"); };
+	void next() { if (chanel->toPlainText().size() <= 3 && QString::number(chanel->toPlainText().toInt() + 1).size() <= 3) chanel->setText(QString::number(chanel->toPlainText().toInt() + 1)); };
+	void prev() { if (chanel->toPlainText().toInt() > 0) chanel->setText(QString::number(chanel->toPlainText().toInt() - 1)); };
+	void volup() { volume->value() + 10 <= 100 ? volume->setValue(volume->value() + 10) : volume->setValue(100); };
+	void voldown() { volume->value() - 10 >= 0 ? volume->setValue(volume->value() - 10) : volume->setValue(0); };
+	void reset() {
+		volume->setValue(0);
+		chanel->clear();
+	}
+};
